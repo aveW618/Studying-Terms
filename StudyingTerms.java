f21e1e9989d69ba6a2da4ecf443ca68d7f76da6e
aveW618 <146094921+aveW618@users.noreply.github.com>
2026-04-11 19:48:18 -0400

    Add files via upload

--- StudyingTerms.java
+++ StudyingTerms.java
@@ -5,7 +5,7 @@ import java.io.*;
 import java.util.*; 
 
 public class StudyingTerms {
-	//declaraing private instance variables
+	//private instance variables
 	private String vocabTerm;
 	private String correctDefinition;
 	private int questionNumber;
@@ -33,10 +33,10 @@ public class StudyingTerms {
 	
 	//creating the loadQuiz method to import the text file and split the info based on delimiters
 		//then printing out the quiz terms, getting input from the user, and outputting their correctness
-	public static ArrayList <String> loadQuiz(String termFile) throws IOException {
+	public static ArrayList <StudyingTerms> loadQuiz(String termFile) throws IOException {
 		//creating a new ArrayList of Objects 
 			//the objects will hold the question, the correct answer, three additional options, and the question number
-		Arraylist <QuizTerm> termList = new ArrayList <QuizTerm>();
+		Arraylist <StudyingTerms> termList = new ArrayList <StudyingTerms>();
 		
 		//file.io code which allows the file to be scanned
 		File myFile = new File(termFile);
@@ -48,7 +48,7 @@ public class StudyingTerms {
 		}
 		
 		//start tracker to keep track of the question number
-		int questionNum = 0;
+		int questionNum = 1;
 		
 		//loop for while the file has a next line/more info
 		while (scan.hasNextLine()) {
@@ -56,33 +56,34 @@ public class StudyingTerms {
 			String line = scan.nextLine();
 			
 			//splits the test on its delimiters
-			String[] terms = line.split("]");
+			String[] data = line.split("]");
 			String[] terms = data[0];
+			String[] definitions = data[1];
 			
-			//I'm still confused about how to store the definition part of the text file
-			//String[] definitions = data[1];
+			termList.add(new StudyingTerms(terms, definitions, questionNum);
+			questionNum++;
+		}
+		scan.close();
 			
-			//looping through the length of the array with the terms stored
-			for (int i = 0; i < terms.length; i++) {
+		//looping through the length of the array with the terms stored
+		for (int i = 0; i < termList.size(); i++) {
 				
-				//uses Math.random to get a random integer from 0 - the array length
-				int index = (Math.random() * terms.length);
+			//uses Math.random to get a random integer from 0 - the array length
+			int index = (Math.random() * terms.length);
 				
-				//increments the question number which will later be outputted to the user
-				questionNum++;
+			//increments the question number which will later be outputted to the user
+			questionNum++;
 				
-				//stores the current term as the word in the terms array at the position of index
-				currentTerm = terms[index];
+			//stores the current term as the word in the terms array at the position of index
+			currentTerm = terms[index];
 				
-				//same as the step above but stores the current defition as the words in the defition array at the position of index
+			//same as the step above but stores the current defition as the words in the defition array at the position of index
 				currentDefition = definitions[index];
 				
 				//adds a term object to the termList ArrayList
 				termList.add(new QuizTerm(questionNum, currentTerm, currentDefinition));
 				
 			}
-			scan.close();
-			
 		}
 	}
 }
@@ -90,9 +91,17 @@ public class StudyingTerms {
 
 //main method
 public static void main(String[] args) throws IOException {
-	ArrayList<QuizTerm> termList = loadQuiz("unit1terms-2.txt");
-	System.out.println("Please type the term correspond to the given definition: ");
-	for (
+	ArrayList<StudyingTerms> termList = loadQuiz("unit1terms-2.txt");
+	//reads input text from user
+	Scanner input = new Scanner(System.in);
+	
+	//counter to track number of correct questions
+	int correct = 0;
+	
+	System.out.println("Please type the term corresponding with the given definition: ");
+
+	for (int i = 0; i < termList.size; i++) {
+			System.out.println("Question #" + questionNum + " " + 
 	
 			
 	
